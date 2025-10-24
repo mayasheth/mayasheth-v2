7fa7eb48208de6366d164970af5c90f5b1658112
Maya Sheth <shethm@F5F7G1QKNY-2.local>
2025-10-24 12:38:58 -0700

    submodule auth

--- scripts/submodule-auth.sh
+++ scripts/submodule-auth.sh
@@ -8,7 +8,7 @@ eval "$(ssh-agent -s)"
 ssh-add /tmp/submodule_deploy.key
 
 # Use the key for all git submodule operations (with verbose output)
-export GIT_SSH_COMMAND="ssh -v -i /tmp/submodule_deploy.key -o IdentitiesOnly=yes"
+export GIT_SSH_COMMAND="ssh -v -i /tmp/submodule_deploy.key -o IdentitiesOnly=yes -o StrictHostKeyChecking=no"
 git submodule update --init --recursive
 
 echo "Listing vault-content directory contents:"
