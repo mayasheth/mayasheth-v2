ed6bc033aa906174f49dfd2d23f235eb09d645f8
Maya Sheth <shethm@F5F7G1QKNY-2.local>
2025-10-24 12:36:49 -0700

    submodule auth

--- scripts/submodule-auth.sh
+++ scripts/submodule-auth.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 set -e
+
+# Start ssh-agent and add the key
 echo "$SUBMODULE_DEPLOY_KEY" > /tmp/submodule_deploy.key
 chmod 600 /tmp/submodule_deploy.key
-eval $(ssh-agent -s)
+eval "$(ssh-agent -s)"
 ssh-add /tmp/submodule_deploy.key
-git submodule update --init --remote
+
+# Use the key for all git submodule operations (with verbose output)
+export GIT_SSH_COMMAND="ssh -v -i /tmp/submodule_deploy.key -o IdentitiesOnly=yes"
+git submodule update --init --recursive
+
+echo "Listing vault-content directory contents:"
+ls -al src/vault-content
+
+
+
